293ca8305b49fab013ae522d4ede6c4c5d62c9dd
Mythri144 <72304576+Mythri144@users.noreply.github.com>
2021-03-21 16:05:41 +0530

    Add files via upload

---
+++ mainP.js
@@ -0,0 +1,26 @@
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.position(700, 445);
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/dObsYho7G/model.json', modelLoaded);
+}
+
+function modelLoaded(){
+    console.log('The model has loaded. ');
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("objectv").innerHTML = results[0].label;
+        document.getElementById("accuracyv").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
